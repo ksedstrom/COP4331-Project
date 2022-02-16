1ec28e3c90cf45a624bb995cfe8287ce7ca081c6
ksedstrom <ksedstrom@outlook.com>
2022-02-16 09:18:29 -0500

    adding bucket game

---
+++ Project/core/src/com/mygdx/game/Drop.java
@@ -0,0 +1,25 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Drop extends Game{
+    public SpriteBatch batch;
+    public BitmapFont font;
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
+
+    }
+
+    public void render(){
+        super.render();
+    }
+
+    public void dispose(){
+        batch.dispose();
+        font.dispose();
+    }
+}
